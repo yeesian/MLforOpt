23fe5e970205387d95155324a203732e234bc183
lroald <lineroald@gmail.com>
2018-01-31 10:57:27 -0700

    Files to run OPF on Darwin

---
+++ RunOPFsamples.jl
@@ -0,0 +1,57 @@
+
+using OPFRecourse, JLD, Clp
+
+
+
+# Running over OPF Benchmark Cases
+for f in [
+        "case3_lmbd",
+        "case5_pjm",
+        "case14_ieee",
+        "case24_ieee_rts",
+        #"case30_as",
+        #"case30_fsr",
+        "case30_ieee",
+        "case39_epri",
+        "case57_ieee",
+        "case73_ieee_rts",
+        # "case89_pegase", # (infeasible)
+        "case118_ieee",
+        "case162_ieee_dtc",
+        "case200_pserc",
+        "case240_pserc",
+        "case300_ieee",
+        # "case1354_pegase", # (infeasible)
+         "case1888_rte",
+         "case1951_rte",
+         # "case2383wp_k" # (infeasible)
+         "case2736sp_k",
+         "case2737sop_k",
+         "case2746wop_k",
+         "case2746wp_k",
+         "case2848_rte",
+         "case2868_rte",
+         "case2869_pegase",
+         "case2869_pegase",
+         "case3012wp_k",
+         "case3120sp_k",
+         "case3375wp_k",
+         "case6468_rte",
+         "case6495_rte",
+         "case6515_rte",
+         "case9241_pegase",
+         "case13659_pegase"
+    ]
+    print("Working on $f: ")
+    data_file = string(Pkg.dir(),"/OPFRecourse/test/data/pglib-opf/pglib_opf_", f, ".m")
+    for i in 1:5
+        @time ref = OPFRecourse.NetworkReference(data_file, σscaling=0.01*i);
+
+        m = OPFRecourse.SingleScenarioOPF(ref, Clp.ClpSolver());
+        srand(1234)
+        scenarios = OPFRecourse.OPFScenarios(ref, m, nsamples = 100);
+        JLD.save("results/$(f)$(i).jld", "scenarios", scenarios)
+        print("$i ")
+    end
+    println()
+end
--- WindowSize.jl
+++ WindowSize.jl
@@ -1,10 +1,19 @@
 # ======================================================================
 """
-Function to calculate window size
-"""
-# ======================================================================
+Function to calculate minimum window size
 
-function WindowSize(alpha, delta, epsilon, gamma)
+Inputs: \\
+delta   - confidence level \\
+epsilon - difference between rate of discovery and probability of unobserved set\\
+gamma   - constant > 1
+
+Outputs:\\
+W_min   - minimium window size\\
+M_min   - number of samples for which we have the minimum window size
+
+"""
+
+function MinWindowSize(delta, epsilon, gamma)
 
     if gamma <= 1
         error("Gamma must be larger than 1!")
@@ -16,3 +25,103 @@ function WindowSize(alpha, delta, epsilon, gamma)
 
     return W_min, M_min
 end
+# ======================================================================
+
+
+
+# ======================================================================
+"""
+Function to calculate window size after M samples
+
+Inputs: \\
+delta   - confidence level \\
+epsilon - difference between rate of discovery and probability of unobserved set\\
+gamma   - constant > 1 \\
+M       - number of samples seen so far
+
+Outputs:\\
+W       - window size\\
+"""
+
+function WindowSize(delta, epsilon, gamma, M)
+
+    if gamma <= 1
+        error("Gamma must be larger than 1!")
+    end
+
+    M_min = 1 + (2*gamma/(delta*(gamma-1)))^(1/(gamma-1));
+
+    W = 2*gamma/epsilon^2*max(log(M_min), log(M));
+
+    return W
+end
+# ======================================================================
+
+
+
+# ======================================================================
+"""
+Calculate rate of discovery where the algorithm terminates
+
+Inputs:\\
+alpha - maximum probbaility of unobserved set\\
+epsilon - difference between rate of discovery and probability of unobserved set
+
+Outputs:\\
+R_MW - rate of discovery where algorithm terminates
+"""
+
+function StoppingCriterion(alpha, delta)
+
+    R_WM = alpha - epsilon
+
+    return R_WM
+end
+# ======================================================================
+
+
+
+# ======================================================================
+"""
+Run learning algorithm for a given system and set of samples
+
+Inputs:\\
+alpha   - maximum unobserved mass \\
+delta   - confidence level \\
+epsilon - difference between rate of discovery and probability of unobserved set\\
+gamma   - constant > 1
+
+
+Outputs:\\
+"""
+
+function RunLearningAlgorithm(alpha, delta, epsilon, gamma)
+
+    # Evaluate stopping criterion
+    R_max = StoppingCriterion(alpha, delta)
+
+    for m=1:maxM
+        # i) Calculate window size for checking
+        W = WindowSize(delta, epsilon, gamma, M)
+
+        # ii) Find observed bases for s=1,...,m
+
+        # iii) Find rate of discovery for s=m+1,...,m+W
+
+        # iv) decide whether to terminate
+        if R_MW <=R_max
+            #end loop
+        end
+    end
+
+    return M, W, R_MW, K_M
+end
+# ======================================================================
+
+
+
+    R_WM = alpha - epsilon
+
+    return R_WM
+end
+# ======================================================================
