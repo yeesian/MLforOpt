10c4a83c5397580ebe6e80a7af72bfdc625058e0
lroald <lineroald@gmail.com>
2018-01-31 17:13:10 -0700

    changing location of pglib-opf

--- RunOPFsamples.jl
+++ RunOPFsamples.jl
@@ -43,13 +43,15 @@ for f in [
          "case13659_pegase"
     ]
     print("Working on $f: ")
-    data_file = string(Pkg.dir(),"/OPFRecourse/test/data/pglib-opf/pglib_opf_", f, ".m")
+    data_file = string("/pglib-opf/pglib_opf_", f, ".m")
+    #data_file = string(Pkg.dir(),"/OPFRecourse/test/data/pglib-opf/pglib_opf_", f, ".m")
     for i in 1:5
         @time ref = OPFRecourse.NetworkReference(data_file, σscaling=0.01*i);
 
         m = OPFRecourse.SingleScenarioOPF(ref, Clp.ClpSolver());
+        #m = OPFRecourse.SingleScenarioOPF(ref, Clp.ClpSolver());
         srand(1234)
-        scenarios = OPFRecourse.OPFScenarios(ref, m, nsamples = 100);
+        scenarios = OPFRecourse.OPFScenarios(ref, m, nsamples = 10);
         JLD.save("results/$(f)$(i).jld", "scenarios", scenarios)
         print("$i ")
     end
