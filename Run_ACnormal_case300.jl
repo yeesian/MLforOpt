4c9b4dcbd3ef544cb29ef1dcc64fb14557c441d5
lroald <lineroald@gmail.com>
2018-06-22 09:34:32 -0600

    run test for case300

---
+++ Run_ACnormal_case300.jl
@@ -0,0 +1,52 @@
+
+using Ipopt
+using Distributions
+using DataStructures
+using JuMP
+using PowerModels
+using ProgressMeter
+using JLD
+
+
+
+# Running over OPF Benchmark Cases
+data_file = "pglib-opf/pglib_opf_case300_ieee.m"
+
+include("find_active_set.jl")
+include("basis_policy.jl")
+include("WindowSize.jl")
+include("LearningAlgorithms_ACOPF.jl")
+
+NLsolver = IpoptSolver(print_level=0)
+
+alpha = 0.6
+delta = 0.01
+epsilon = 0.3
+gamma = 2
+Minitial = 1
+
+M, W, RoD, K_M, results = RunStreamingAlgorithmAC(alpha, delta, epsilon, gamma, Minitial, data_file, NLsolver, tol = 1e-5)
+
+JLD.save("results_ACnormal_case300.jld", "results", results, "M", M, "W", W, "RoD", RoD, "K_M", K_M)
+
+println()
+println("!!!!!!!!!!!!!!! FINISHED WORKING ON $f !!!!!!!!!!!!!!!!!!!!")
+println()
+
+#for i=3 #in 1:5
+#     @time ref = OPFRecourse.NetworkReference(data_file, σscaling=0.01*i);
+#     m = OPFRecourse.SingleScenarioOPF(ref, Gurobi.GurobiSolver());
+#
+#     # Generate new uncertainties based on uniform distribution
+#     srand(1234)
+#     nsamples = 50_000
+#     ωsamples = zeros(ref.nbus, nsamples)
+#     nonzeroindices = (1:length(ref.stdω))[ref.stdω .> 1e-5]
+#     for b in nonzeroindices
+#         ωsamples[b,:] = rand(Uniform(-3*ref.stdω[b], 3*ref.stdω[b]), nsamples)
+#     end
+#     # solve OPF for uniform distribution realizations
+#     scenarios = OPFRecourse.OPFScenarios(ref, m, ωsamples);
+#     JLD.save("results_uniform/$(f)$(i).jld", "scenarios", scenarios)
+#     print("$i ")
+# #end
